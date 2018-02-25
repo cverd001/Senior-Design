f798abcfd362555cf8e2de227fd3a2375e65465f
yidiwang21 <ydwang21@gmail.com>
2018-02-25 05:06:03 +0000

    add communication with teensy in computer vision remove old version code filter out unwanted contour sizes

--- rpi/scanArea.py
+++ rpi/scanArea.py
@@ -5,27 +5,42 @@ import picamera.array
 import numpy as np
 from picamera import PiCamera
 from picamera.array import PiRGBArray
-#from matplotlib import pyplot as plt
+import RPi.GPIO as GPIO
 
+sigPin = 11
+ctrlPin = 13
+
+camera = PiCamera()
+camera.resolution = (640,480)
+camera.framerate = 30
+camera.hflip = True
+img_size = (640,480)
+rawCapture = PiRGBArray(camera,size=img_size)
+boundaries = [
+    ([200,200,200],[255,255,255])]
+
+spotsMax = 0
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(sigPin, GPIO.OUT)
+    GPIO.setup(ctrlPin, GPIO.IN)
+    GPIO.output(sigPin, GPIO.LOW)
+     
 def previewCam(camera):
+    global spotsMax
     for frame in camera.capture_continuous(rawCapture,format='bgr',use_video_port=True):
         img = frame.array
-
+        img = img[:, 240:400]
         for (lower, upper) in boundaries:
             lower = np.array(lower,dtype="uint8")
-            upper = np.array(upper,dtype="uint8")
+            upper = np.array(upper,dtype="uint8") 
             mask = cv2.inRange(img,lower,upper)
             bw = cv2.bitwise_and(img,img,mask=mask)
             
-        
         blur = cv2.GaussianBlur(bw,(5,5),0)
         gray = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
         edge = cv2.Canny(gray,40,80,L2gradient=True)
-        #ret, thresh = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
-        
-        #kernel = np.ones((5,5))
-        #opening = cv2.morphologyEx(edge,cv2.MORPH_OPEN, kernel)
-        #closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
         
         output = bw.copy()
         _, contours, hierachy = cv2.findContours(edge,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
@@ -38,9 +53,10 @@ def previewCam(camera):
             epsilon = 0.1*cv2.arcLength(cnt,True)
             approx = cv2.approxPolyDP(cnt,epsilon,True)
             perimeter = cv2.arcLength(cnt,True)
-            '''
-            if area < 30 or area > 1500:
+    
+            if area < 500 or area > 8000:
                 continue
+            '''
             if perimeter < 30 or perimeter > 1000:
                 continue
             '''
@@ -48,9 +64,12 @@ def previewCam(camera):
                 max_area = area
                 max_perimeter = perimeter
                 curr_contour = cnt
-
-            #ellipse = cv2.fitEllipse(cnt)
-            #cv2.ellipse(output,ellipse,(0,255,0),2)
+                if spotsMax < max_area:  # found a new max spots
+                    GPIO.output(sigPin, GPIO.HIGH)
+                    time.sleep(1)
+                    GPIO.output(sigPin, GPIO.LOW)
+                    spotsMax = max_area
+                    print("Signal sent! Freshing spotsMax to : ", spotsMax)
            
             cv2.drawContours(output,curr_contour,-1,(0,255,0),3)
 
@@ -59,44 +78,17 @@ def previewCam(camera):
         cv2.waitKey(100) 
         rawCapture.truncate(0)
 
-        
-    #    if cv2.contourArea(prev_contour) <= cv2.contourArea(curr_contour):
-    #       # cv2.drawContours(output,curr_contour,-1,(0,255,0),3)
-    #        prev_contour = curr_contour
-    #        return True
-    #    else:
-    #        return False
-            
-           # ellipse = cv2.fitEllipse(cnt)
-           # cv2.ellipse(output,ellipse,(0,255,0),2)
-           
-            
+def destroy():
+    GPIO.output(sigPin, GPIO.LOW)
+    GPIO.cleanup()
+    spotsMax = 0
 
-camera = PiCamera()
-camera.resolution = (640,480)
-camera.framerate = 30
-#camera.vflip = True
-camera.hflip = True
-img_size = (640,480)
-rawCapture = PiRGBArray(camera,size=img_size)
-
-boundaries = [
-    ([200,200,200],[255,255,255])]
-
-def runMain():
-    time.sleep(1)
-    val = 0
-    for spot in range(10):
-        if previewCam(camera) == True:
-            val = val + 1
-    if val >= 8:
-          print('good')
-    else:
-        print('bad')
-
-if __name__=="__main__":
+if __name__ == '__main__':
+    setup()
     while True:
-        #runMain()
-        previewCam(camera)
-
-    
+        # previewCam(camera)
+        if GPIO.input(ctrlPin) == 0:
+            print("Control signal from teensy received, opening camera...")
+            previewCam(camera)
+        # else:
+        #     time.sleep(100)
