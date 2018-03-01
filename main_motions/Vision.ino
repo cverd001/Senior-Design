ed24f91edba2aaab3f3c1bb5358b9451dbd8ef04
YidiWang <ydwang21@gmail.com>
2018-03-01 13:01:40 -0800

    embedded traverse alg into vision part

--- main_motions/Vision.ino
+++ main_motions/Vision.ino
@@ -6,23 +6,24 @@
 //teensy2piPin
 //pi2teensyPin
 
+int spotflag = 0;
 
-void startScan(){
-  digitalWrite(teensy2piPin,HIGH);  //start signal
+void startScan() {
+  digitalWrite(teensy2piPin, HIGH);  //start signal
   laserTone();
   Serial.println("Sending start signal to pi");
 }
 
-void stopScan(){
-  digitalWrite(teensy2piPin,LOW);  //start signal
+void stopScan() {
+  digitalWrite(teensy2piPin, LOW);  //start signal
   Serial.println("Sending stop signal to pi");
 }
 
-void headToLight(){
+void headToLight() {
   moveForward();
   while(true){
-    int lightness=readSolarVoltage();
-    if (lightness >=120){
+    int lightness = readSolarVoltage();
+    if (lightness >= 120){
       break;
     }
   }
@@ -33,13 +34,13 @@ void headToLight(){
 
 
 
-void turnRightBurstImu(){
+void turnRightBurstImu() {
   int i = 0;
   int maxDegId = 0;
-  float temp=0;
+  float temp = 0;
   Serial.println("Initializing IMU...");
   mpu.resetFIFO();
-  for(int i = 0; i <175; i++) {
+  for(int i = 0; i < 175; i++) {
     testIMU();
   }
   testIMU();
@@ -49,9 +50,10 @@ void turnRightBurstImu(){
     calMoveRight(20);
     Serial.print("Right turn number: ");
     Serial.println(i);
-    if(digitalRead(pi2teensyPin)==HIGH){
+    if(digitalRead(pi2teensyPin) == HIGH){
       bootTone();
-      maxDegId=i;
+      maxDegId = i;
+      spotflag = 1;
       Serial.print("Raspberry Pi sent HIGH signal! New max set to ");
       Serial.println(i);
     }
@@ -65,14 +67,20 @@ void scan(){
   startScan();
   turnRightBurstImu();  //finds best angle
   delay(30000);
-  //headToLight();        //goes to light
-  //Spin();
-  victoryTone();
-  Tracking(); //fix
+  stopScan();
+  if(spotflag == 1) {   // spots detected, do heading 
+    //headToLight();        //goes to light
+    //Spin();
+    victoryTone();
+    Tracking(); //fix
+  }
+  else {                // no spots found, do perform_traverse
+    PerformTraverseAlg();
+  }
 }
 
 void scanPlus(){
-  for(int i = 0; i <1100; i++) {
+  for(int i = 0; i < 1100; i++) {
     testIMU();
     Serial.println(i);
   }
